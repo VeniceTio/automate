d4ab2dd37848a0eeb51ffc8f7de0da5d7727585c
Tony Le <tle@unistra.fr>
2019-12-31 13:37:17 +0100

    maj settings window: mise en place de gridbaglayout

--- src/view/SettingsWindow.java
+++ src/view/SettingsWindow.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class SettingsWindow extends JFrame {
-
     //All the options for the game
     private ArrayList<String> _gameOptions = new ArrayList<>(Arrays.asList("Game of life", "Fredkin n°1", "Fredkin n°2"));
     private ArrayList<String> _expansionOptions = new ArrayList<>(Arrays.asList("Repetition", "Periodicity", "Symetry n°1", "Symetry n°2", "Constant"));
@@ -19,142 +18,164 @@ public class SettingsWindow extends JFrame {
     public SettingsWindow() {
         //Settings of the settings window
         setTitle("Settings window");
-        setSize(420, 300);
+        setSize(500, 400);
         setLocationRelativeTo(null);
+        setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         //The panel containing everything
-        JPanel _settingsWindow = (JPanel) getContentPane();
-        _settingsWindow.setLayout(new BorderLayout());
-
-        //The panel containing the contents of the settings window
-        JPanel contents = new JPanel(new GridLayout(6, 1, 10, 10));
-        contents.setBorder(new EmptyBorder(0, 10, 0, 10));
-
-        //Adding the elements to the pane
-        contents.add(createLabel("Grid's size : "));
-        contents.add(createTextField(10));
-
-        contents.add(createLabel("Games's extension : "));
-        contents.add(createComboBox(_expansionOptions.toArray()));
-
-        contents.add(createLabel("Games's speed : "));
-        contents.add(createTextField(10));
-        
-        //contents.add(createGamesOption(_gameOptions, 2));
-
-        contents.add(createLabel("Number of turns"));
-        contents.add(createTextField(10));
-
-        contents.add(createLabel("Number of cells"));
-        contents.add(createTextField(10));
+        JPanel settingsWindow = (JPanel) getContentPane();
+        settingsWindow.setLayout(new BorderLayout());
 
         //Header of the settings window
-        JPanel header = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JLabel titleLabel = new JLabel("Choose the settings for your game ! ");
-        header.add(titleLabel);
-        _settingsWindow.add(header, BorderLayout.NORTH);
+        settingsWindow.add(createHeader(), BorderLayout.NORTH);
 
         //Contents of the settings window
-        _settingsWindow.add(contents, BorderLayout.CENTER);
+        settingsWindow.add(createSettingsContents(), BorderLayout.CENTER);
 
         //Footer of the settings windows
-        JButton quitButton = new JButton("Quit");
-        JButton validateButton = new  JButton("Validate");
-        quitButton.setPreferredSize(new Dimension(90, 30));
-        validateButton.setPreferredSize(new Dimension(90, 30));
-
-        JPanel footer = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        footer.setBorder(new EmptyBorder(0, 0, 0, 5));
-        footer.add(quitButton);
-        footer.add(validateButton);
-        _settingsWindow.add(footer, BorderLayout.SOUTH);
-
-        //Adding the action event for the elements
-        quitButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                System.exit(0);
-            }
-        });
-
-        validateButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                validateGame();
-            }
-        });
+        settingsWindow.add(createFooter(), BorderLayout.SOUTH);
 
         //Settings the content pane of the settings window
-        setContentPane(_settingsWindow);
+        setContentPane(settingsWindow);
     }
 
-    private JLabel createLabel(String text) { //TODO
+    private JLabel createLabel(String text) {
         return new JLabel(text);
     }
 
-    private JTextField createTextField(int size) { //TODO
+    private JTextField createTextField(int size) {
         return new JTextField(size);
     }
 
-    private JComboBox<?> createComboBox(Object[] values) { //TODO
-        return new JComboBox<>(values);
+    private JComboBox<Object> createComboBox(Object[] values, ActionListener al) {
+        JComboBox<Object> cbo = new JComboBox<>(values);
+        cbo.setPreferredSize(new Dimension(20, 10));
+        cbo.addActionListener(al);
+        return cbo;
     }
 
-    private JPanel createGamesOption(ArrayList<String> gameOptions, int numPlayer) {
-        JPanel playerPanel = new JPanel(new GridLayout(numPlayer, 2));
+    private JButton createButton(String text, int width, int height, ActionListener al) {
+        JButton button = new JButton(text);
+        button.setPreferredSize(new Dimension(width, height));
+        button.addActionListener(al);
+        return button;
+    }
 
-        for(int i = 1; i < numPlayer; i++) {
-            playerPanel.setBorder(new EmptyBorder(0, 0,0, 0));
-            JLabel playerLabel = new JLabel("Player n°" + numPlayer + " : ");
-            JComboBox<String> cboGameOptions  = new JComboBox(gameOptions.toArray());
+    private JPanel createHeader() {
+        JPanel header = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        header.add(createLabel("Choose the settings for your game !"));
+        return header;
+    }
 
-            //Adding the event selectedIndexChanged for the combobox
-            cboGameOptions.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent actionEvent) {
-                    selectedIndexChanged(actionEvent);
-                }
-            });
+    private JPanel createSettingsContents() {
+        JPanel settingsContents = new JPanel(new GridBagLayout());
+        settingsContents.setBorder(new EmptyBorder(0, 0, 0, 0));
 
-            playerPanel.add(playerLabel);
-            playerPanel.add(cboGameOptions);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.insets = new Insets(5, 5, 5, 5);
+
+        gbc.weightx = 5;
+        gbc.weighty = 5;
+
+        //Adding the elements to the pane
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        settingsContents.add(createLabel("Grid's size : "), gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        settingsContents.add(createTextField(10), gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        settingsContents.add(createLabel("Games's extension : "), gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        settingsContents.add(createComboBox(_expansionOptions.toArray(), null), gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        settingsContents.add(createLabel("Games's speed : "), gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        settingsContents.add(createTextField(10), gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        settingsContents.add(createLabel("Number of turns : "), gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        settingsContents.add(createTextField(10), gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        settingsContents.add(createGamesOption(_gameOptions, 2), gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 5;
+        settingsContents.add(createLabel("Number of cells : "), gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 5;
+        settingsContents.add(createTextField(10), gbc);
+
+
+        return settingsContents;
+    }
+
+    private JPanel createFooter() {
+        JPanel footer = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        footer.setBorder(new EmptyBorder(0, 0, 0, 5));
+        footer.add(createButton("Quit", 90, 30, actionEvent -> System.exit(0)));
+        footer.add(createButton("Validate", 90, 30, actionEvent -> confirmSettings()));
+        return footer;
+    }
+
+    private JPanel createGamesOption(ArrayList<String> gameOptions, int numPlayer) { //TODO: change the size of the cells
+        JPanel gamesOptionPanel = new JPanel(new GridLayout(numPlayer, 2, 14,10));
+        gamesOptionPanel.setBorder(new EmptyBorder(0, 0,0, 0));
+
+        for(int i = 1; i <= numPlayer; i++) {
+            gamesOptionPanel.add(createLabel("Player n°" + i + " : "));
+            gamesOptionPanel.add(createComboBox(gameOptions.toArray(), actionEvent -> updateGameOptions(actionEvent)));
         }
 
-        return playerPanel;
+        return gamesOptionPanel;
     }
 
-    private void selectedIndexChanged(ActionEvent e) { //TODO
-        JPanel contents = (JPanel) getContentPane().getComponent(1);
+    private void updateGameOptions(ActionEvent e) { //TODO: problem here
         JComboBox<String> cboSelected = (JComboBox) e.getSource();
-        JPanel t = (JPanel) cboSelected.getParent();
-
-        for(Component c : t.getComponents()) {
-            System.out.println(c);
-        }
+        JPanel contents = (JPanel) cboSelected.getParent();
 
         String gameOptionSelected = (String) cboSelected.getSelectedItem();
         ArrayList<String> gameOptionsRemaining = new ArrayList<>();
 
-        for(String s : _gameOptions) {
+        for(String s: _gameOptions) {
             if(!s.equals(gameOptionSelected)) {
                 gameOptionsRemaining.add(s);
             }
         }
 
+        System.out.println(gameOptionsRemaining);
+
         for(Component c : contents.getComponents()) {
-            if((c instanceof JComboBox) && (!c.equals(cboSelected)) ) {
+            if((c instanceof JComboBox) && (!c.equals(cboSelected))) {
                 JComboBox<String> cbo = (JComboBox) c;
                 cbo.removeAllItems();
-                for(String s : gameOptionsRemaining) {
+                for(String s: gameOptionsRemaining) {
                     cbo.addItem(s);
                 }
             }
         }
-
     }
 
-    private void validateGame() { //TODO
+    private void confirmSettings() { //TODO
     }
 
     public static void main(String[] args) {
