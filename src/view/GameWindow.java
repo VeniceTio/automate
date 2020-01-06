3eb0e5b89fa3b3aa3077212ea2985ebdd76d29cc
ylouazel <yoann.louazel@iutrs.unistra.fr>
2020-01-06 23:25:43 +0100

    maj GameWindow + uml

--- src/view/GameWindow.java
+++ src/view/GameWindow.java
@@ -13,15 +13,25 @@ import java.util.ArrayList;
 public class GameWindow extends JFrame implements Observer {
 
 
-
-    //TODO: commentaire à faire
     /**
-     * Le nombre de cellule par joueur
+     * Le nombre de cellule par joueur au depart
      */
     private final int _startCell;
+    /**
+     * est en initialisé
+     */
     private boolean _init = false;
+    /**
+     * couleur des joueurs
+     */
     private final Color[] _players;
+    /**
+     * liste des bouttons representant les cellules
+     */
     private final ArrayList<MyButton> _cells = new ArrayList<>();
+    /**
+     * Nombre aleatoire pour les combat
+     */
     private static final SecureRandom _rand = new SecureRandom();
 
     /**
