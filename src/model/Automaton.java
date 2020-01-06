c5f38061ba11319a4af095c10412fdbebc856798
ylouazel <yoann.louazel@iutrs.unistra.fr>
2020-01-06 21:50:31 +0100

    maj Expansion Automaton

--- src/model/Automaton.java
+++ src/model/Automaton.java
@@ -8,11 +8,22 @@ public enum Automaton {
     FREDKIN1("Fredkin n°1"),
     FREDKIN2("Fredkin n°2");
 
+    /**
+     * version string de l'enumeration
+     */
     private final String _abreviation;
 
+    /**
+     * Constructeur d'enumeration
+     * @param abreviation version string de l'enumeration
+     */
     Automaton(String abreviation){
         this._abreviation = abreviation;
     }
 
+    /**
+     * Méthode renvoyant la version string de l'enumeration
+     * @return version string de l'enumeration
+     */
     public String getAbreviation(){return _abreviation;}
 }
